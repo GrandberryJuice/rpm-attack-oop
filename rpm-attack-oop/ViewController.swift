268ea40c0a5d33ca13555b7a3571d215481ab11b
kenneth Grandberry <tbkots2@yahoo.com>
2015-11-17 18:15:59 -0600

    Initial Commit

---
+++ rpm-attack-oop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpm-attack-oop
+//
+//  Created by KENNETH GRANDBERRY on 11/17/15.
+//  Copyright © 2015 KENNETH GRANDBERRY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
